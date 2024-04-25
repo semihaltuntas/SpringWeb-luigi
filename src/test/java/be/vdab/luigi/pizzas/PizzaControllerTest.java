53512ead06af5d4ae244352992598e92e2fc6a41
semihkenan <altuntas.semih34@gmail.com>
2024-04-25 13:25:01 +0200

    3.dag SpringWeb

---
+++ src/test/java/be/vdab/luigi/pizzas/PizzaControllerTest.java
@@ -0,0 +1,42 @@
+package be.vdab.luigi.pizzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Sql("/pizzas.sql")
+@AutoConfigureMockMvc
+class PizzaControllerTest {
+    private final static String PIZZAS_TABLE = "pizzas";
+    private final MockMvc mockMvc;
+    private final JdbcClient jdbcClient;
+
+    public PizzaControllerTest(MockMvc mockMvc, JdbcClient jdbcClient) {
+        this.mockMvc = mockMvc;
+        this.jdbcClient = jdbcClient;
+    }
+
+    @Test
+    void findAantalVindtHetJuisteAantalPizzas() throws Exception {
+//        int  aantalPizzas = JdbcTestUtils.countRowsInTable(jdbcClient, PIZZAS_TABLE);
+//        System.out.println(aantalPizzas);
+        mockMvc.perform(get("/pizzas/aantal"))
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$")
+                                .value(JdbcTestUtils.countRowsInTable(jdbcClient, PIZZAS_TABLE)));
+    }
+}
